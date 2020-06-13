883bbbe2bdec39e6f0c853383910a213e054ffd4
zhiyin1314 <66709571+zhiyin1314@users.noreply.github.com>
2020-06-13 08:35:07 +0800

    Update config.php

--- 万维网/config.php
+++ 万维网/config.php
@@ -1,7 +1,7 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyDJ_B0IFehXUqV-MZTdPnh3FNPRGzl6c6U');//这里改成您的api key
-define('GJ_CODE', 'KR');//国家代码
+define('GJ_CODE',HK');//国家代码
 define('SITE_NAME', 'aerjieta');//网站名字
 define('TITLENAME', 'aerjieta');//网站名字
 define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
